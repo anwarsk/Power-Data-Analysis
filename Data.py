8d7cbdf772de40006040fb4bf66b5003dc098012
Anwar Shaikh <mail2anwarshaikh@gmail.com>
2016-05-05 03:32:39 -0400

    Created Data Class

---
+++ Data.py
@@ -0,0 +1,50 @@
+'''
+Created on Mar 15, 2016
+ 
+ This module is holding all the data for the program
+  
+@author: Anwar
+'''
+
+''' Portfolio List '''
+portfolios = []
+
+
+d = []
+E = []
+CF = []
+
+''' Vectors'''
+R = []
+TCR_o = []
+FOM_o = []    
+VOM_o = []    
+n_o = []
+Q_o = []  
+EF = []
+COF = []
+PR_TCR = []
+PR_FOM = []
+PR_VOM = []
+PR_n = []
+LR = []
+J = []
+B = []
+
+''' ScalarA '''
+ACC_0 = 0
+r = 0
+m = 0
+mu = 0
+sigma = 0
+kappa = 0
+lamda = 0
+ceta = 0
+
+
+''' Calculated Data '''
+T = 0
+W_out = []
+Q_out = []
+E_out = []
+TCR_out = []
